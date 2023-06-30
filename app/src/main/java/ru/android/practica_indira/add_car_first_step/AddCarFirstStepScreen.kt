31478d5fa36b1ca0884440380f15b78cbcd971cf
Deadboiklek <vselenskyryadovoy@gmail.com>
2023-06-30 15:22:24 +0800

    Ебаный рот наворотил хуеты ;) охуеешь

--- app/src/main/java/ru/android/practica_indira/add_car_first_step/AddCarFirstStepScreen.kt
+++ app/src/main/java/ru/android/practica_indira/add_car_first_step/AddCarFirstStepScreen.kt
@@ -7,18 +7,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -75,17 +80,22 @@ fun AddCarFirstStepScreen() {
                 }
             }
             item { 
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(horizontal = 37.dp)
+                    ) {
                     AddCarMenu(
                         menuName = "Марка автомобиля", 
                         modifier = Modifier.padding(top = 48.dp)
                     )
                     Row(
                         horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding(top = 4.dp)
+                        modifier = Modifier
+                            .padding(top = 4.dp)
                     ) {
-                        AddCarMenu(menuName = "Модель")
-                        AddCarMenu(menuName = "Год")
+                        AddCarMenu(menuName = "Модель", Modifier.weight(5f))
+                        Spacer(modifier = Modifier.padding(4.dp))
+                        AddCarMenu(menuName = "Год", Modifier.weight(2f))
                     }
                     AddCarMenu(
                         menuName = "Мощность двигателя",
@@ -150,7 +160,30 @@ fun AddCarMenu(
             expanded = isExpanded,
             onExpandedChange = { isExpanded = it }
         ) {
-            TextField(
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                color = Color(0xFFF7F7F7),
+                shape = MaterialTheme.shapes.extraSmall
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = item.takeIf {
+                            it.isNotEmpty()
+                        } ?: menuName,
+
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .menuAnchor()
+                    )
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = null,)
+                }
+
+            }
+            /*TextField(
                 value = item,
                 onValueChange = {},
                 readOnly = true,
@@ -160,7 +193,7 @@ fun AddCarMenu(
                 colors = ExposedDropdownMenuDefaults.textFieldColors(),
                 modifier = Modifier.menuAnchor(),
                 placeholder = { Text(text = menuName)}
-            )
+            )*/
             ExposedDropdownMenu(
                 expanded = isExpanded,
                 onDismissRequest = { isExpanded = false }
--- app/src/main/java/ru/android/practica_indira/main_screen/ui/MainScreen.kt
+++ app/src/main/java/ru/android/practica_indira/main_screen/ui/MainScreen.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
